5e7d266cac996aa75ca2cfa3eebf9748a6405c19
Karina_Belxhenko <karin.bel4enko@yandex.ru>
2022-12-07 17:21:59 +0300

    для Д/З/

---
+++ lesson2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.Write("Введи цифру, обозначающую день недели: ");
+int []Arrau = {6, 7, 1};
+
+int n = Arrau.Length;
+
+
+void FillArrau (int[]collection);
+
+void PrinntArray(int []col);
+
+
+int count = dayNumbe;
+
+int position = 0;
+
+while(position < count);
+{
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("это вообще не день недели");
+  }
+  else Console.WriteLine("(этот день не выходной) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
+
+
+
+
+
+
