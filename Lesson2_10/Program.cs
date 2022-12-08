2b1ce4f30f16650a4eb51ce8b38a723d97543272
Karina_Belxhenko <karin.bel4enko@yandex.ru>
2022-12-08 18:00:33 +0300

    для Д/З/

---
+++ Lesson2_10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int value = 645; 
+int a = value / 100;
+int b = (value / 10) % 10 ;
+int C = value % 10;
+Console.WriteLine(c);
+
+int value = 78; 
+int a = value / 100;
+int b = (value / 10) % 10 ;
+int C = value % 10;
+Console.WriteLine(c);
+
+int value = 32679; 
+int a = value / 100;
+int b = (value / 10) % 10 ;
+int C = value % 10;
+Console.WriteLine(b);
+
