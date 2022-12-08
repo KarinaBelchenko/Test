1ea913941837629788c2dff4340cd81c67f9d7f6
Karina_Belxhenko <karin.bel4enko@yandex.ru>
2022-12-08 16:34:23 +0300

    для Д/З/

--- lesson2_13/Program.cs
+++ lesson2_13/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цмфры нет  
+// 456 -> 5 
+//782 -> 8
+// 918 -> 1
+
+
+int value = 456; 
+int a = value / 100;
+int b = (value / 10) % 10 ;
+int C = value % 10;
+Console.WriteLine(b);
+
+int value1 = 782; 
+int d = value1 / 100;
+int e = (value1 / 10) % 10 ;
+int f = value1 % 10;
+Console.WriteLine(e);
+
+int value2 = 918; 
+int g = value2 / 100;
+int h = (value2 / 10) % 10 ;
+int I = value2 % 10;
+Console.WriteLine(h);
+
+
+
+
+
+
+
+ 
+
