9c88c23c1ba2106834371be49fb7a8e94b81bd65
Karina_Belxhenko <karin.bel4enko@yandex.ru>
2022-12-18 19:00:33 +0300

    для Д/З/

---
+++ AAA/Program.cs
@@ -0,0 +1,69 @@
+﻿//Задайте массив заполненный случайными положительными трехзначными числами.
+// Напишите программу, которая покажет количество четных чисел в массиве.
+
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArray(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintGood (numbers);
+
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+
+// 0. Метод, считывающий число от пользователя из консоли
+int GetNumber(string text)
+{
+  Console.Write(text);
+  int value = int.Parse(Console.ReadLine()!);
+  return value;
+}
+
+// 1. метод, создающий массив с указанным количеством эл.
+int[] CreateArray(int count)
+{
+  return new int[count];
+}
+
+int[] arr = CreateArray(new int{});
+
+// метод заполнякт массив
+    
+void FillArray(int[] array)
+{
+  int size = array.Length;
+
+  int index = 0;
+  while (index < size)
+  {
+    array[index] = GetNumber($"array[{index}]: ");
+    index++;
+  }
+}
+
+ // печать массива 
+string PrintGood (int[] numbers)
+{
+ int count = numbers.Length;
+ int i = 0; 
+string result = "[ ";
+
+while (i < count)
+{
+    result += ($"{numbers[i], 3} ");
+    i++;
+}
+return result + " ]";
+}
+
+
+
+
+
--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,5 +1,20 @@
-﻿// Задача 29: Напишите методы, который задаёт массив из 8 элементов и выводит их на экран.
+﻿
+//[345, 897, 568, 234] -> 2
 
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+
+
+int GetNumber(string text)
+{
+  Console.Write(text);
+  int value = int.Parse(Console.ReadLine()!);
+  return value;
+}
 
 // 1. метод, создающий массив с указанным количеством эл.
 int[] CreateArray(int count)
@@ -11,32 +26,84 @@ int[] arr = CreateArray(new int{});
 
 // метод заполнякт массив
 
-void FillArray(int[] array)
 
+void FillArray(int[] array)
 {
-    int Length = array.Length;
+int Length = array.Length;
     int index = 0;
     while (index < Length)
 
-{
+
     array[index] = new Random().Next(1, 8);
     index++;
 }
+    
 
+// 3. метод, подсчёта четных элементов
+int CountIsEven(int[] collection)
+{
+  int result = 0;
+
+  int size = collection.Length;
+  int i = 0;
+  while (i < size)
+  {
+    if (collection[i] % 2 == 0) result++;
+    i++;
   }
+
+  return result;
+}
+int[] Solution(int[] collectionElements, int evenCounts)
+{
+
+  int[] items = new int[evenCounts];
+  int length = collectionElements.Length;
+  int position = 0;
+  int indexInItems = 0;
+  while (position < length)
+  {
+    if (collectionElements[position] % 2 == 0)
+    {
+      items[indexInItems] = collectionElements[position];
+      indexInItems++;
+    }
+    position++;
+  }
+
+  return items;
+}
+
  // печать массива 
-void PrintArray (int[] collection)
+string PrintGood (int[] collection)
 {
-    int count = collection.Length;
-    for (int i = 0; i < count; i++)
+ int count = collection.Length;
+ int i = 0; 
+string result = "[ ";
+
+while (i < count)
 {
-Console.WriteLine($"{collection[i]} ");
-
+    result += ($"{collection[i], 3} ");
+    i++;
 }
-Console.WriteLine(); 
+return result + " ]";
 }
 
-int[] array = new int[8] {1, 2, 5, 7, 19, 6, 1, 33};
 
+int size = Convert.ToInt32(Console.ReadLine());
+int countCollection = GetNumber("Введите количество эл массива: ");
+int[] array = CreateArray(countCollection);
 FillArray(array);
-PrintArray(array);
+PrintGood(array);
+int even = CountIsEven(array);
+int[] newArr = Solution(arr, even);
+Console.WriteLine(PrintGood(array));
+
+
+
+int count = 0;
+
+for (int z = 0; z < array.Length; z++)
+if (array[z] % 2 == 0)
+count++;
+
